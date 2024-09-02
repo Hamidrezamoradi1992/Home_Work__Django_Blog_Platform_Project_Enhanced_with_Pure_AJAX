d21ebad2b473b2d96ba7e3c4669fbb6344506101
Hamidrezamoradi1992 <hamidreza1992@gmail.com>
2024-09-02 20:17:31 +0330

    create models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
+from django.core import validators
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authore'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        ordering = ['updated_at']
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
